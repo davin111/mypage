331293edf2566e2d052181c6187bb51c0dae7efd
Davin <bdv951101@gmail.com>
2019-04-02 16:07:31 +0900

    made templates of Post app and used the templates (index and details of posts)

--- posts/views.py
+++ posts/views.py
@@ -2,22 +2,29 @@ from django.shortcuts import render
 
 
 from django.http import HttpResponse
-from .models import Post
+from django.http import Http404
 
-import datetime
-from django.utils import timezone
+from .models import Post
+from django.template import loader
+
+from django.shortcuts import render, get_object_or_404
+
+#import datetime
+#from django.utils import timezone
 
 def index(request):
-    dt = timezone.localtime()
-    now = dt.strftime("%Y.%m.%d. %H:%M:%S (%A)")
+    #dt = timezone.localtime()
+    #now = dt.strftime("%Y.%m.%d. %H:%M:%S (%A)")
 
     latest_post_list = Post.objects.order_by('-pub_date')[:5] #show recent 5 posts, ordered by 'pub_date'
-    output = ', '.join([p.content for p in latest_post_list])
+    context = {'latest_post_list': latest_post_list}
 
-    return HttpResponse("Hello, world! Now is " + now + " (Seoul) [posts]\n" + output)
+    return render(request, 'posts/index.html', context)
 
 def detail(request, post_id):
-    return HttpResponse("You're looking at post %s." % post_id)
+    post = get_object_or_404(Post, pk=post_id)
+    
+    return render(request, 'posts/detail.html', {'post': post})
 
 def results(request, post_id):
     response = HttpResponse("You're looking at the results of post %s." % post_id)
