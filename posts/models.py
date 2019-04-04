020d646f1984e2c71288540d42cdc4e4c8ecd375
Davin <bdv951101@gmail.com>
2019-04-04 14:27:38 +0900

    made things about User

--- posts/models.py
+++ posts/models.py
@@ -2,12 +2,16 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
+from django.contrib.auth.models import User
+
 class Post(models.Model):
     content = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+    author = models.ForeignKey(User, on_delete = models.CASCADE, related_name='user_posts')
+
     def __str__(self):
-        return self.content
+        return self.author.username + " " + self.content
 
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
--- posts/urls.py
+++ posts/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
 
+app_name = 'posts'
+
 urlpatterns = [
     path('', views.index, name = 'index'),
     path('<int:post_id>/', views.detail, name = 'detail'),
     path('<int:post_id>/results/', views.results, name = 'results'),
     path('<int:post_id>/react/', views.react, name = 'react'),
+    #url(r'^api-auth/', include('rest_framework.urls')),
 ]
